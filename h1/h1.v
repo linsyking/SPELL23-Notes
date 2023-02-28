626b4fcccb3b4f83bcf7da15ba495d3b6f50fcde
linsy king <linsy_king@sjtu.edu.cn>
2023-03-01 00:22:41 +0800

    p2

--- h1/h1.v
+++ h1/h1.v
@@ -39,3 +39,30 @@ apply H1.
 intro H4.
 apply H3.
 Qed.
+
+Theorem t_18: forall P Q: Prop, ((~P->~Q)->(~P->Q)->P)->~~P->P.
+Proof.
+unfold not.
+intros P Q H1 H2.
+apply H1.
+intros H3 H4.
+apply H3.
+elim H2.
+intro H5.
+apply H3.
+apply H5.
+intro H3.
+elim H2.
+apply H3.
+Qed.
+
+(* Alternative approach *)
+Theorem t_18_alt: forall P: Prop, ((~P->P)->P)->~~P->P.
+Proof.
+unfold not.
+intros P H1 H2.
+apply H1.
+intro H3.
+elim H2.
+apply H3.
+Qed.
